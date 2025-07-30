33e3ca5e43da5f75cc943deb6a255629903e8290
fifah-l <fifahr493@gmail.com>
2025-07-30 17:55:06 +0700

    Menambahkan page jenjang di sidebar

--- src/views/pagesekolah/berita/CardBerita.js
+++ src/views/pagesekolah/berita/CardBerita.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import "../../../css/berita/cardBerita.css"
 
-const CardBerita = ({ image, title, content, id, link }) => {
+const CardBerita = ({ image, title, content, id, link, date }) => {
     return (
         <div className='card-berita'>
             <img src={image} alt={title} />
-            <h4 style={{ textTransform: "uppercase" }}>{title}</h4>
-            <p className="content-berita" style={{ fontSize: "14px", textAlign: "justify" }}>
+            <h4 style={{ textTransform: "uppercase", color: "#000 !important" }}>{title}</h4>
+            <p className="date-berita" style={{ color: "#000 !important" }}>{date}</p>
+            <div className="content-berita" style={{ color: "#000 !important" }}>
                 <div
                     dangerouslySetInnerHTML={{ __html: content }}
+                    style={{ color: "inherit !important" }}
                 />
-            </p>
-            <a href={`/detail-${link}-${id}`}>Selengkapnya</a>
+            </div>
+            <a href={`/detail-${link}-${id}`} style={{ color: "#000 !important" }}>Selengkapnya</a>
         </div>
     );
 };
