6d1c955b588c534b4edf7da0b4fa28fcdb28a196
fatiya salsabila <fatiyasalsabila994@gmail.com>
2026-06-19 13:26:21 +0700

    udpate

--- src/utils/base_URL.js
+++ src/utils/base_URL.js
@@ -1,2 +1,2 @@
-//  export const API_DUMMY = "http://localhost:2027";
-export const API_DUMMY = "https://api.slbcpelitailmu.sch.id";
+//  export const API_DUMMY = "http://localhost:9900";
+export const API_DUMMY = "https://api.smpn1bergas.sch.id";
